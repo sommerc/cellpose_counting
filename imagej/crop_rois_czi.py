0ea2cbaf10078cf1d2e0a75181887db51ce293ba
Christoph Sommer <christoph.sommer@ist.ac.at>
2020-08-10 17:04:58 +0200

    integrated z-slice selection for roi export

--- imagej/crop_rois_czi.py
+++ imagej/crop_rois_czi.py
@@ -4,8 +4,10 @@
 #@ String (visibility=MESSAGE, value="Channels will be split and files for each ROI in RoiManager will be saved as tif") msg2
 #@ String (visibility=MESSAGE, value="<html><b>Mapping of filters from CZI metadata</b><br/><ol><li>Hoechst 33342 &nbsp;: blue</li><li>Alexa Fluor 488 : green</li><li>Alexa Fluor 594 : red</li><li>Alexa Fluor 647 : yellow</li></ol></html>") msg3
 #@ String (label="Color for segmentation", choices={"blue", "green", "red", "yellow"}, style="listBox") seg_choice
-#@ Integer (label="CellPose diamteter", value=30) diameter
+#@ Integer (label="Export channels with z-slices +-", value=1, min=0, max=3) z_rng
 #@ boolean (value=false, label="Copy CellPose run command to clipboard") run_cp
+#@ Integer (label="CellPose diamteter", value=30) diameter
+
 
 __author__ = "christoph.sommer@ist.ac.at"
 
@@ -51,10 +53,18 @@ def get_roi_images(imp):
         return
         
     rois = list(RoiManager.getInstance().getRoisAsArray())
+
+    if len(rois) == 0:
+        IJ.showMessage("Add ROIs to RoiManager first")
+        return
+    
     res = []
+
+    NZ = imp.getNSlices()
+    
     for roi in rois:
         imp.setRoi(roi)
-        roi_img = Duplicator().run(imp, 1, imp.getNChannels(), roi.getZPosition(), roi.getZPosition(), 1, 1) 
+        roi_img = Duplicator().run(imp, 1, imp.getNChannels(), max(roi.getZPosition()-z_rng, 1), min(NZ, roi.getZPosition()+ z_rng), 1, 1) 
         local_roi = roi.clone()
         local_roi.setLocation(0,0)
         roi_img.setRoi(local_roi)
@@ -78,9 +88,15 @@ def main():
         im_channels = ChannelSplitter().split(im)
         for c, ch_img in enumerate(im_channels):
             out_fn = os.path.join(str(out_dir), "{}_ROI_{:02d}_{}.tif".format(fn_base, r, channel_colors[c+1]))
+
+            # segmentation channel only single slice
+            if channel_colors[c+1] == seg_choice:
+                central_slice = int(ch_img.getNSlices() / 2 + 1)
+                ch_img = Duplicator().run(ch_img, 1, 1, central_slice, central_slice, 1, 1)
+            
             IJ.saveAs(ch_img, "Tiff", out_fn)
         
-    os.system("explorer {}".format(out_dir))
+    #os.system("explorer {}".format(out_dir))
 
     if run_cp:
         import subprocess
